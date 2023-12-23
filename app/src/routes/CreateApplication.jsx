77fbd396d84aad7a872309e9209f9889f197d58a
Dan Bowles <daniel.bowles@gmail.com>
2023-12-22 20:09:14 -0500

    :recycle: Reworked how the form worked, broke it out into component

--- app/src/routes/CreateApplication.jsx
+++ app/src/routes/CreateApplication.jsx
@@ -1,55 +1,83 @@
 import React, { useState } from 'react';
 import Header from "../components/Header"
+import AlertBanner from '../components/banners/AlertBanner';
+import SuccessBanner from '../components/banners/SuccessBanner';
+import ApplicantFormComponents from '../components/ApplicantFormComponents';
+import { isEveryPropertyTruthy, buttonClasses } from '../utils';
 
 const CreateApplication = () => {
-  const [formData, setFormData] = useState({
+  const [applicantData, setApplicantData] = useState({
     firstName: '',
     lastName: '',
     dateOfBirth: '',
   });
 
+  const [newApplicationId, setNewApplicationId] = useState(0);
+  const [error, setError] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+
   const handleChange = (event) => {
-    setFormData({
-      ...formData,
+    setError(null);
+    setApplicantData({
+      ...applicantData,
       [event.target.name]: event.target.value,
     });
   };
 
+  isEveryPropertyTruthy(applicantData)
+  const formValid = isEveryPropertyTruthy(applicantData)
+    && !error
+    && !submitting;
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setSubmitting(true);
     try {
-      const response = await fetch('http://localhost:3000/application', {
+      await fetch('http://localhost:3000/application', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(applicantData),
+      }).then(res => {
+        if (!res.ok) throw res;
+        setSubmitting(false);
+        return res.json();
+      }).then(data => {
+        setNewApplicationId(data.newApplicationId);
+      }).catch(error => {
+        error.json().then(err => {
+          setSubmitting(false);
+          setError(err);
+        });
       });
 
-      const data = await response.json();
-      console.log(data);
-      // const redirectUrl = data.redirectUrl || '/'; // Use a default URL if not provided
-      // window.location.href = redirectUrl;
     } catch (error) {
       console.error('Error submitting form:', error);
-      // Handle errors gracefully, e.g., display an error message to the user
     }
   };
 
   return (
     <>
       <Header headerText="👋 Welcome" subTitleText="Fill out this short form to start your application!" />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} />
-        <br />
-        <label htmlFor="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} />
-        <br />
-        <label htmlFor="dateOfBirth">Date of Birth:</label>
-        <input type="date" id="dateOfBirth" name="dateOfBirth" value={formData.dateOfBirth} onChange={handleChange} />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <p className="mb-5 font-bold">ℹ️ Please note, all fields are required</p>
+      {error && error.message && <AlertBanner message={error.message} />}
+      {!newApplicationId ?
+        <form onSubmit={handleSubmit}>
+          <ApplicantFormComponents formData={applicantData} onChange={handleChange} />
+          <div className="flex justify-end w-full">
+            <button disabled={!formValid} className={buttonClasses(!formValid)}>
+              Create Application
+            </button>
+          </div>
+        </form >
+        :
+        <div className="">
+          <SuccessBanner message="🎉 Your application has been created!" />
+          <p className="mt-5">
+            <span>Follow this link below to resume your application:  </span>
+            <a href={`/application/${newApplicationId}`} className="hover:underline text-[#6A64F1] text-base font-semibold">Resume Application →</a>
+          </p>
+        </div>
+      }
     </>
   )
 }
